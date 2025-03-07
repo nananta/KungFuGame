06f4af5e9b5722f1ce8c68ca248fc1d042cbadda
Nitinan Ananta <nitinanananta@MacBookAir.attlocal.net>
2025-03-06 22:07:27 -0800

    Add local development server

---
+++ server_local.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// Serve index.html for all routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`
+    🎮 Game server running!
+    
+    Local:            http://localhost:${PORT}
+    On Your Network:  http://${getLocalIP()}:${PORT}
+    
+    Press Ctrl+C to stop the server
+    `);
+});
+
+// Helper function to get local IP address
+function getLocalIP() {
+    const { networkInterfaces } = require('os');
+    const nets = networkInterfaces();
+    
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+            if (net.family === 'IPv4' && !net.internal) {
+                return net.address;
+            }
+        }
+    }
+    return 'localhost';
+} 
